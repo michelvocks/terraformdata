dee7944d28792fce8b2800cbe1b38d78ec50cdaf
Michel Vocks <michel.vocks@dbschenker.com>
2017-03-01 16:50:49 +0100

    Added default data collection as a first test

---
+++ main.tf
@@ -0,0 +1,45 @@
+data "aws_vpc" "vpc" {
+  filter {
+    name = "tag:Name"
+
+    values = [
+      "${var.aws_tags["topic"]}-${var.aws_tags["stage"]}-vpc",
+    ]
+  }
+}
+
+data "aws_subnet" "web_eu_central_1a" {
+  filter {
+    name = "tag:Name"
+
+    values = [
+      "${var.aws_tags["topic"]}-${var.aws_tags["stageold"]}-sn-web-euc1a",
+    ]
+  }
+
+  vpc_id = "${data.aws_vpc.vpc.id}"
+}
+
+data "aws_subnet" "web_eu_central_1b" {
+  filter {
+    name = "tag:Name"
+
+    values = [
+      "${var.aws_tags["topic"]}-${var.aws_tags["stageold"]}-sn-web-euc1b",
+    ]
+  }
+
+  vpc_id = "${data.aws_vpc.vpc.id}"
+}
+
+data "aws_subnet" "app_eu_central_1a" {
+  filter {
+    name = "tag:Name"
+
+    values = [
+      "${var.aws_tags["topic"]}-${var.aws_tags["stageold"]}-sn-app-euc1a",
+    ]
+  }
+
+  vpc_id = "${data.aws_vpc.vpc.id}"
+}
