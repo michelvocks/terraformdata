3da44ad07c1a2bc82df470a720e2ca236f4a34ae
Michel Vocks <michel.vocks@dbschenker.com>
2017-03-02 09:18:29 +0100

    Fixed outputs

--- outputs.tf
+++ outputs.tf
@@ -1,19 +1,29 @@
-output "vpc" {
-  value = "${data.aws_vpc.vpc}"
+##### ID outputs #####
+output "vpc_id" {
+  value = "${data.aws_vpc.vpc.id}"
 }
 
-output "web_eu_central_1a" {
-  value = "${data.aws_subnet.web_eu_central_1a}"
+output "web_eu_central_1a_id" {
+  value = "${data.aws_subnet.web_eu_central_1a.id}"
 }
 
-output "web_eu_central_1b" {
-  value = "${data.aws_subnet.web_eu_central_1b}"
+output "web_eu_central_1b_id" {
+  value = "${data.aws_subnet.web_eu_central_1b.id}"
 }
 
-output "app_eu_central_1a" {
-  value = "${data.aws_subnet.app_eu_central_1a}"
+output "app_eu_central_1a_id" {
+  value = "${data.aws_subnet.app_eu_central_1a.id}"
 }
 
-output "app_eu_central_1b" {
-  value = "${data.aws_subnet.app_eu_central_1b}"
+output "app_eu_central_1b_id" {
+  value = "${data.aws_subnet.app_eu_central_1b.id}"
+}
+
+##### CIDR outputs #####
+output "web_eu_central_1a_cidr" {
+  value = "${data.aws_subnet.web_eu_central_1a.cidr_block}"
+}
+
+output "web_eu_central_1b_cidr" {
+  value = "${data.aws_subnet.web_eu_central_1b.cidr_block}"
 }
