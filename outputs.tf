e8a6e64cdd8d53312c28feec73e56704e843b7c7
Michel Vocks <michel.vocks@dbschenker.com>
2017-05-03 14:56:58 +0200

    Added app_cidr outputs

--- outputs.tf
+++ outputs.tf
@@ -48,6 +48,14 @@ output "web_eu_central_1b_cidr" {
   value = "${data.aws_subnet.web_eu_central_1b.cidr_block}"
 }
 
+output "app_eu_central_1a_cidr" {
+  value = "${data.aws_subnet.app_eu_central_1a.cidr_block}"
+}
+
+output "app_eu_central_1b_cidr" {
+  value = "${data.aws_subnet.app_eu_central_1b.cidr_block}"
+}
+
 output "tools_app_eu_central_1a_cidr" {
   value = "${data.aws_subnet.tools_app_eu_central_1a.cidr_block}"
 }
