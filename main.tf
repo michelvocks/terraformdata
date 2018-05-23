db8d91efb35aa3fd2b9740589c78d9abb2189f0b
Michel Vocks <michel.vocks@dbschenker.com>
2018-05-23 09:02:33 +0200

    Added base ami linux

--- main.tf
+++ main.tf
@@ -165,3 +165,9 @@ data "aws_ami" "base_ami" {
   owners      = ["self"]
   name_regex  = "dis-ami-base"
 }
+
+data "aws_ami" "base_ami_linux" {
+  most_recent = true
+  owners      = ["self"]
+  name_regex  = "dis-ami-aws-linux-base"
+}
